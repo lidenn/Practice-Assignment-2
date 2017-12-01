0b195c63c84d1b5534adad16e8ca8e195b40b62b
Shaklore <Thomashuynh257@gmail.com>
2017-11-30 16:55:40 -0800

    Took out cout statements and it now writes to an output file

--- lights2/lights.cpp
+++ lights2/lights.cpp
@@ -1,3 +1,4 @@
+//Group members: Bradford Wong, Dennis Li, Thomas Huynh
 #include "./cnf2sat.hpp"
 #include <iostream>
 #include <fstream>
@@ -19,7 +20,7 @@ int main(){
       file >> number_switches;
       file.ignore();
       file >> number_bulbs;
-      cout << "There are " << number_switches << " switches, bulbs: " << number_bulbs << endl;
+      //cout << "There are " << number_switches << " switches, bulbs: " << number_bulbs << endl;
       struct bulb  bulbs[number_bulbs];
       int initialState[number_bulbs];
 
@@ -36,9 +37,8 @@ int main(){
                   file.ignore();
             }
             file >> initialState[i];
-           cout << initialState[i] << ","; //uncomment if want to see initial states
+           //cout << initialState[i] << ","; //uncomment if want to see initial states
       }
-cout << endl;
 
       /*Read in which switches are connected to what bulbs and pass them into the array of bulbs*/
       string temporaryString = "";
@@ -48,7 +48,7 @@ cout << endl;
             string temp = "";
             string s;
             getline(file, s);
-            cout << "Just read in the line: " << s << endl;
+            //cout << "Just read in the line: " << s << endl;
             for(int i = 0; i < s.length(); i++){
             //      cout << "End of string is " << s[s.length()] << endl;
                   //cout << "string length is " << s.length() << endl;
@@ -79,48 +79,19 @@ cout << endl;
                   }
             }
             //cout << "Line " << line << ": ";
-            /*
-            while(atEnd == false){
-                  if(file.peek() == ','){
-                        file.ignore();
-                  }
 
-                  int tempBulb;
-                  file >> tempBulb;
-                  cout << tempBulb << ", ";
-                  tempBulb--;
-                  if(bulbs[tempBulb].switch1 == -9999999){
-                        bulbs[tempBulb].switch1 = line + 1;
-                  }else{
-                        bulbs[tempBulb].switch2 = line + 1;
-                  }
 
-                  if(file.eof() || file.peek() == '\n')
-                        atEnd = true;
-
-            }*/
-            cout << endl;
       }
       /*Set up vector of 2SAT statements. */
       vector<pair<int, int> > test;
       for(int i = 0; i < number_bulbs; i++){
             //cout << i << endl;
             if(initialState[i] == 1){
-                  //test.push_back(make_pair(bulbs[i].switch1 * -1, bulbs[i].switch2));
-                  //cout << "Pushed " << bulbs[i].switch1 * -1 << ", " << bulbs[i].switch2 << endl;
-                //  test.push_back(make_pair(bulbs[i].switch1, bulbs[i].switch2 * -1));
-                  //cout << "Pushed " <<bulbs[i].switch1 << ", " <<  bulbs[i].switch2 * -1 << endl;
-                  //swapped content of if and else statements
                   test.push_back(make_pair(bulbs[i].switch1, bulbs[i].switch2));
             //      cout << "Pushed " << bulbs[i].switch1  << ", " << bulbs[i].switch2 << endl;
                   test.push_back(make_pair(bulbs[i].switch1*-1, bulbs[i].switch2*-1));
             //      cout << "Pushed " <<bulbs[i].switch1 * -1 << ", " <<  bulbs[i].switch2 * -1 << endl;
             }else{
-                  //test.push_back(make_pair(bulbs[i].switch1, bulbs[i].switch2));
-            //      cout << "Pushed " << bulbs[i].switch1  << ", " << bulbs[i].switch2 << endl;
-                  //test.push_back(make_pair(bulbs[i].switch1*-1, bulbs[i].switch2*-1));
-            //      cout << "Pushed " <<bulbs[i].switch1 * -1 << ", " <<  bulbs[i].switch2 * -1 << endl;
-            //swapped content of if and else statements
                     test.push_back(make_pair(bulbs[i].switch1 * -1, bulbs[i].switch2));
                     //cout << "Pushed " << bulbs[i].switch1 * -1 << ", " << bulbs[i].switch2 << endl;
                     test.push_back(make_pair(bulbs[i].switch1, bulbs[i].switch2 * -1));
@@ -132,45 +103,23 @@ cout << endl;
       for(int i = 0; i < number_bulbs; i++){
             cout << " Bulb " << i << " is connected to " << bulbs[i].switch1 << ", " << bulbs[i].switch2 << endl;
       }*/
-      cout << "Answer is: " << satisfiable(test) << endl;
+      //cout << "Answer is: " << satisfiable(test) << endl;
+      string answer;
+      ofstream output_file("output.txt");
+      if (satisfiable(test) == 0){
+        answer = "no";
+        output_file << answer << "\n";
+      }
+      else{
+        answer = "yes";
+        output_file << answer << "\n";
+      }
+
       test.clear();
-      //cout << "At end of for loop " << endl;
-      /*
-
-
-            test.push_back(make_pair(-1, 3));
-            test.push_back(make_pair(1, -3));
-            test.push_back(make_pair(-3, 4));
-            test.push_back(make_pair(3, -4));
-            test.push_back(make_pair(-1, 4));
-            test.push_back(make_pair(1, -4));
-            test.push_back(make_pair(1, 2));
-            test.push_back(make_pair(-1, -2));
-            test.push_back(make_pair(-3, 4));
-            test.push_back(make_pair(3, -4));
-            cout << "Answer is: " << satisfiable(test) << endl;
-            //
-            /*
-            string temp;
-            getline(file, temp);
-            cout << temp << endl;
-            vector<int> vect;
-            stringstream ss(temp);
-
-            int i;
-            while(ss >> i){
-                  vect.push_back(i);
-
-                  if(ss.peek() == ',')
-                        ss.ignore();
-            }
-            temp = "";
-            */
+      output_file.close();
 
 
 
-
-      cout << endl;
 
       file.close();
       return 0;
